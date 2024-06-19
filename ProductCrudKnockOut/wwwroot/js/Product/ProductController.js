0a82dca832e596cc0c28aaff65b958cee1455b5e
Gyalwang Ghhyamjo lama <gyalwanglama123@gmail.com>
2024-06-19 09:12:30 +0545

    Product CRUD done

--- ProductCrudKnockOut/wwwroot/js/Product/ProductController.js
+++ ProductCrudKnockOut/wwwroot/js/Product/ProductController.js
@@ -5,8 +5,12 @@ var productController = function (item) {
     const baseUrl = "/api/ProductApi";
 
     self.Products = ko.observableArray([]);
+    self.selectedProduct = ko.observable(new productModel());
 
     self.NewProduct = ko.observable(new productModel());
+    self.isupdated = ko.observable(false);
+    self.isEditing = ko.observable(false);
+
 
     self.getData = function () {
         var url = baseUrl;
@@ -29,9 +33,58 @@ var productController = function (item) {
 
 
     //Add Product
+    self.AddProduct = function () {
+        if (self.isEditing()) {
+            self.EditProduct(); // Call the EditProduct function if in edit mode
+        }
+        else {
+            ajax.post(baseUrl + "/Add", ko.toJSON(self.NewProduct())).done(function (result) {
+                console.log(result);
+                self.Products.push(new productModel(result));
+                self.resetForm();
 
+                location.reload();
+            })
+        }
+    };
+
+    //Edit Product
+    self.EditProduct = function () {
+        ajax.put(baseUrl + "/" + self.NewProduct().id(), ko.toJSON(self.NewProduct())).done(function (result) {
+            console.log(result);
+            self.Products.replace(self.selectedProduct(), new productModel(result));
+            self.resetForm();
+
+            location.reload();
+        });
+    };
+
+    //Delete Product
+
+    self.DeleteProduct = function (model) {
+        ajax.delete(baseUrl + "?id=" + model.id()).done((result) => {
+            self.Products.remove(model);
+        })
+    };
+
+
+
+
+    self.selectProduct = (model) => {
+        self.selectedProduct(model);
+        self.NewProduct(new productModel(ko.toJS(model)));
+        self.isupdated(true);
+        self.isEditing(true);
+    };
+
+    self.resetForm = function () {
+        self.NewProduct(new productModel());
+        self.isupdated(false);
+        self.isEditing(false); // Reset editing mode to false
+    };
 
 }
+
 
 var ajax = {
     get: function (url) {
@@ -40,5 +93,36 @@ var ajax = {
             url: url,
             async: false,
         });
+    },
+    post: function (url, data) {
+        return $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            url: url,
+            data: (data)
+        })
+    },
+
+    put: function (url, data) {
+        return $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'COntent-Type': 'application/json'
+            },
+            method: "PUT",
+            url: url,
+            data: data
+        });
+    },
+
+    delete: function (route) {
+        return $.ajax({
+            method: "DELETE",
+            url: route,
+        });
     }
+
 };
